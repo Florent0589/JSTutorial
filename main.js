e7a5ccbb87f7129f3dff31edd03841c04f6f4234
Ayanda Mavimbela <ayandam@smartz-solutions.com>
2021-06-02 19:15:09 +0200

    Dom manipulation

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var mysql      = require('mysql');
+require('dotenv').config()
+
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : ''
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE
 });
 
 
